f273319478efbffdd3662c16b336677fa7d0799d
Shadi Abu Hilal <shadiabuhilal@Shadis-MacBook-Pro-2.local>
2023-12-06 17:49:46 -0800

    Adding docs

--- index.cjs
+++ index.cjs
@@ -1,8 +1,30 @@
 'use strict';
 
+/**
+ * @private
+ * @description Supported JSON content type values ['application/json', 'application/problem+json', 'application/vnd.api+json', 'application/hal+json'].
+ */
+const jsonContentTypes = [
+    'application/json', 
+    'application/problem+json', 
+    'application/vnd.api+json', 
+    'application/hal+json'
+];
+
+/**
+ * @function
+ * @description Function that check if current request is a json request or not.
+ * @param {Object} req - Http request object
+ * @param {Object} req.headers - Http request headers object
+ * @param {string} req.headers."content-type" - Http request content-type header
+ * @returns {boolean}
+ */
 const isJsonReq = (req)=> {
     const contentType = req && req.headers && req.headers['content-type'];
-    if (contentType && contentType.toLowerCase().includes('application/json')) {
+
+    const normalizedContentType = contentType && contentType.toLowerCase();
+
+    if (normalizedContentType && jsonContentTypes.some((value)=> normalizedContentType.includes(value))) {
         return true;
     }
 
@@ -10,16 +32,49 @@ const isJsonReq = (req)=> {
 };
 
 const checkMethod = (req, method)=> {
-    if (method && method.toLowerCase() === method) {
+    const normalizedReqMethod  = req && req.method && req.method && req.method.toLowerCase();
+    
+    if (normalizedReqMethod === method) {
         return true;
     }
 
     return false;
 };
 
+/**
+ * @function
+ * @description Function that check if current request is a http GET method request or not.
+ * @param {Object} req - Http request object
+ * @param {string} req.method - Http request method string
+ * @returns {boolean}
+ */
 const isGetReq = (req)=> checkMethod(req, 'get');
+
+/**
+ * @function 
+ * @description Function that check if current request is a http POST method request or not.
+ * @param {Object} req - Http request object
+ * @param {string} req.method - Http request method string
+ * @returns {boolean}
+ */
 const isPostReq = (req)=> checkMethod(req, 'post');
+
+/**
+ * @function
+ * @description Function that check if current request is a http PUT method request or not.
+ * @param {Object} req - Http request object
+ * @param {string} req.method - Http request method string
+ * @returns {boolean}
+ */
 const isPutReq = (req)=> checkMethod(req, 'put');
+
+/**
+ * @function 
+ * @description Function that check if current request is a http DELETE method request or not.
+ * @param {Object} req - Http request object
+ * @param {string} req.method - Http request method string
+ * @returns {boolean}
+ */
 const isDeleteReq = (req)=> checkMethod(req, 'delete');
 
 exports.isDeleteReq = isDeleteReq;
--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-const jsonContentTypesArr = [
+/**
+ * @private
+ * @description Supported JSON content type values ['application/json', 'application/problem+json', 'application/vnd.api+json', 'application/hal+json'].
+ */
+const jsonContentTypes = [
     'application/json', 
     'application/problem+json', 
     'application/vnd.api+json', 
     'application/hal+json'
 ];
 
+/**
+ * @function
+ * @description Function that check if current request is a json request or not.
+ * @param {Object} req - Http request object
+ * @param {Object} req.headers - Http request headers object
+ * @param {string} req.headers."content-type" - Http request content-type header
+ * @returns {boolean}
+ */
 export const isJsonReq = (req)=> {
     const contentType = req && req.headers && req.headers['content-type'];
 
     const normalizedContentType = contentType && contentType.toLowerCase();
 
-    if (normalizedContentType && jsonContentTypesArr.some((value)=> normalizedContentType.includes(value))) {
+    if (normalizedContentType && jsonContentTypes.some((value)=> normalizedContentType.includes(value))) {
         return true;
     }
 
@@ -27,7 +39,38 @@ const checkMethod = (req, method)=> {
     return false;
 };
 
+/**
+ * @function
+ * @description Function that check if current request is a http GET method request or not.
+ * @param {Object} req - Http request object
+ * @param {string} req.method - Http request method string
+ * @returns {boolean}
+ */
 export const isGetReq = (req)=> checkMethod(req, 'get');
+
+/**
+ * @function 
+ * @description Function that check if current request is a http POST method request or not.
+ * @param {Object} req - Http request object
+ * @param {string} req.method - Http request method string
+ * @returns {boolean}
+ */
 export const isPostReq = (req)=> checkMethod(req, 'post');
+
+/**
+ * @function
+ * @description Function that check if current request is a http PUT method request or not.
+ * @param {Object} req - Http request object
+ * @param {string} req.method - Http request method string
+ * @returns {boolean}
+ */
 export const isPutReq = (req)=> checkMethod(req, 'put');
+
+/**
+ * @function 
+ * @description Function that check if current request is a http DELETE method request or not.
+ * @param {Object} req - Http request object
+ * @param {string} req.method - Http request method string
+ * @returns {boolean}
+ */
 export const isDeleteReq = (req)=> checkMethod(req, 'delete');
