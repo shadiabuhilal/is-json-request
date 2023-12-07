950e5263ef466269e3a90232d23ae357453dd49d
Shadi Abu Hilal <shadiabuhilal@Shadis-MacBook-Pro-2.local>
2023-12-06 18:06:14 -0800

    Add eslint and semantic-release

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
  * @description Supported JSON content type values ['application/json', 'application/problem+json', 'application/vnd.api+json', 'application/hal+json'].
  */
 const jsonContentTypes = [
-    'application/json', 
-    'application/problem+json', 
-    'application/vnd.api+json', 
+    'application/json',
+    'application/problem+json',
+    'application/vnd.api+json',
     'application/hal+json'
 ];
 
@@ -17,21 +17,21 @@ const jsonContentTypes = [
  * @param {string} req.headers."content-type" - Http request content-type header
  * @returns {boolean}
  */
-export const isJsonReq = (req)=> {
+export const isJsonReq = (req) => {
     const contentType = req && req.headers && req.headers['content-type'];
 
     const normalizedContentType = contentType && contentType.toLowerCase();
 
-    if (normalizedContentType && jsonContentTypes.some((value)=> normalizedContentType.includes(value))) {
+    if (normalizedContentType && jsonContentTypes.some((value) => normalizedContentType.includes(value))) {
         return true;
     }
 
     return false;
 };
 
-const checkMethod = (req, method)=> {
+const checkMethod = (req, method) => {
     const normalizedReqMethod  = req && req.method && req.method && req.method.toLowerCase();
-    
+
     if (normalizedReqMethod === method) {
         return true;
     }
@@ -46,16 +46,16 @@ const checkMethod = (req, method)=> {
  * @param {string} req.method - Http request method string
  * @returns {boolean}
  */
-export const isGetReq = (req)=> checkMethod(req, 'get');
+export const isGetReq = (req) => checkMethod(req, 'get');
 
 /**
- * @function 
+ * @function
  * @description Function that check if current request is a http POST method request or not.
  * @param {Object} req - Http request object
  * @param {string} req.method - Http request method string
  * @returns {boolean}
  */
-export const isPostReq = (req)=> checkMethod(req, 'post');
+export const isPostReq = (req) => checkMethod(req, 'post');
 
 /**
  * @function
@@ -64,13 +64,13 @@ export const isPostReq = (req)=> checkMethod(req, 'post');
  * @param {string} req.method - Http request method string
  * @returns {boolean}
  */
-export const isPutReq = (req)=> checkMethod(req, 'put');
+export const isPutReq = (req) => checkMethod(req, 'put');
 
 /**
- * @function 
+ * @function
  * @description Function that check if current request is a http DELETE method request or not.
  * @param {Object} req - Http request object
  * @param {string} req.method - Http request method string
  * @returns {boolean}
  */
-export const isDeleteReq = (req)=> checkMethod(req, 'delete');
+export const isDeleteReq = (req) => checkMethod(req, 'delete');
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,131 +1,132 @@
+/* globals describe, it */
 import expect from 'expect.js';
-import {isJsonReq, isGetReq, isPostReq, isPutReq, isDeleteReq } from '../index.js';
+import { isJsonReq, isGetReq, isPostReq, isPutReq, isDeleteReq } from '../index.js';
 
 describe('is-json-request util', () => {
     describe('isJsonReq()', () => {
-      it('should return true', () => {
-        expect(
-            isJsonReq({
-            headers: {
-                'content-type': 'application/json'
-            }
-        })
-        ).to.be.ok();
+        it('should return true', () => {
+            expect(
+                isJsonReq({
+                    headers: {
+                        'content-type': 'application/json'
+                    }
+                })
+            ).to.be.ok();
 
-        expect(
-            isJsonReq({
-            headers: {
-                'content-type': 'application/json; charset=utf-8'
-            }
-        })
-        ).to.be.ok();
+            expect(
+                isJsonReq({
+                    headers: {
+                        'content-type': 'application/json; charset=utf-8'
+                    }
+                })
+            ).to.be.ok();
 
-        expect(
-            isJsonReq({
-            headers: {
-                'content-type': 'application/problem+json'
-            }
-        })
-        ).to.be.ok();
+            expect(
+                isJsonReq({
+                    headers: {
+                        'content-type': 'application/problem+json'
+                    }
+                })
+            ).to.be.ok();
 
-        expect(
-            isJsonReq({
-            headers: {
-                'content-type': 'application/vnd.api+json'
-            }
-        })
-        ).to.be.ok();
+            expect(
+                isJsonReq({
+                    headers: {
+                        'content-type': 'application/vnd.api+json'
+                    }
+                })
+            ).to.be.ok();
 
-        expect(
-            isJsonReq({
-            headers: {
-                'content-type': 'application/hal+json'
-            }
-        })
-        ).to.be.ok();
-      });
+            expect(
+                isJsonReq({
+                    headers: {
+                        'content-type': 'application/hal+json'
+                    }
+                })
+            ).to.be.ok();
+        });
 
-      it('should return false', () => {
-        expect(
-            isJsonReq()
-        ).to.not.be.ok();
+        it('should return false', () => {
+            expect(
+                isJsonReq()
+            ).to.not.be.ok();
 
-        expect(
-            isJsonReq({})
-        ).to.not.be.ok();
+            expect(
+                isJsonReq({})
+            ).to.not.be.ok();
 
-        expect(
-            isJsonReq({ headers: null })
-        ).to.not.be.ok();
+            expect(
+                isJsonReq({ headers: null })
+            ).to.not.be.ok();
 
-        expect(
-            isJsonReq({ headers: {} })
-        ).to.not.be.ok();
+            expect(
+                isJsonReq({ headers: {} })
+            ).to.not.be.ok();
 
-        expect(
-            isJsonReq({
-            headers: {
-                'content-type': null
-            }
-        })
-        ).to.not.be.ok();
+            expect(
+                isJsonReq({
+                    headers: {
+                        'content-type': null
+                    }
+                })
+            ).to.not.be.ok();
 
-        expect(
-            isJsonReq({
-            headers: {
-                'content-type': ''
-            }
-        })
-        ).to.not.be.ok();
+            expect(
+                isJsonReq({
+                    headers: {
+                        'content-type': ''
+                    }
+                })
+            ).to.not.be.ok();
 
-        expect(
-            isJsonReq({
-            headers: {
-                'content-type': 'text/plain'
-            }
-        })
-        ).to.not.be.ok();
+            expect(
+                isJsonReq({
+                    headers: {
+                        'content-type': 'text/plain'
+                    }
+                })
+            ).to.not.be.ok();
 
-        expect(
-            isJsonReq({
-            headers: {
-                'content-type': 'text/javascript'
-            }
-        })
-        ).to.not.be.ok();
+            expect(
+                isJsonReq({
+                    headers: {
+                        'content-type': 'text/javascript'
+                    }
+                })
+            ).to.not.be.ok();
 
-        expect(
-            isJsonReq({
-            headers: {
-                'content-type': 'application/xml'
-            }
-        })
-        ).to.not.be.ok();
+            expect(
+                isJsonReq({
+                    headers: {
+                        'content-type': 'application/xml'
+                    }
+                })
+            ).to.not.be.ok();
 
-        expect(
-            isJsonReq({
-            headers: {
-                'content-type': 'application/octet-stream'
-            }
-        })
-        ).to.not.be.ok();
+            expect(
+                isJsonReq({
+                    headers: {
+                        'content-type': 'application/octet-stream'
+                    }
+                })
+            ).to.not.be.ok();
 
-        expect(
-            isJsonReq({
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded'
-            }
-        })
-        ).to.not.be.ok();
+            expect(
+                isJsonReq({
+                    headers: {
+                        'content-type': 'application/x-www-form-urlencoded'
+                    }
+                })
+            ).to.not.be.ok();
 
-        expect(
-            isJsonReq({
-            headers: {
-                'content-type': 'multipart/form-data'
-            }
-        })
-        ).to.not.be.ok();
-      });
+            expect(
+                isJsonReq({
+                    headers: {
+                        'content-type': 'multipart/form-data'
+                    }
+                })
+            ).to.not.be.ok();
+        });
     });
 
     describe('isGetReq()', () => {
@@ -133,25 +134,25 @@ describe('is-json-request util', () => {
             expect(
                 isGetReq({
                     method: 'get'
-            })
+                })
             ).to.be.ok();
 
             expect(
                 isGetReq({
                     method: 'Get'
-            })
+                })
             ).to.be.ok();
 
             expect(
                 isGetReq({
                     method: 'GEt'
-            })
+                })
             ).to.be.ok();
 
             expect(
                 isGetReq({
                     method: 'GET'
-            })
+                })
             ).to.be.ok();
         });
 
@@ -159,25 +160,25 @@ describe('is-json-request util', () => {
             expect(
                 isGetReq()
             ).to.not.be.ok();
-    
+
             expect(
                 isGetReq({})
             ).to.not.be.ok();
-    
+
             expect(
                 isGetReq({ method: null })
             ).to.not.be.ok();
-    
+
             expect(
                 isGetReq({ method: '' })
             ).to.not.be.ok();
-    
+
             expect(
                 isGetReq({
                     method: 'post'
-            })
+                })
             ).to.not.be.ok();
-      });
+        });
     });
 
     describe('isPostReq()', () => {
@@ -185,25 +186,25 @@ describe('is-json-request util', () => {
             expect(
                 isPostReq({
                     method: 'post'
-            })
+                })
             ).to.be.ok();
 
             expect(
                 isPostReq({
                     method: 'Post'
-            })
+                })
             ).to.be.ok();
 
             expect(
                 isPostReq({
                     method: 'POst'
-            })
+                })
             ).to.be.ok();
 
             expect(
                 isPostReq({
                     method: 'POST'
-            })
+                })
             ).to.be.ok();
         });
 
@@ -211,51 +212,51 @@ describe('is-json-request util', () => {
             expect(
                 isPostReq()
             ).to.not.be.ok();
-    
+
             expect(
                 isPostReq({})
             ).to.not.be.ok();
-    
+
             expect(
                 isPostReq({ method: null })
             ).to.not.be.ok();
-    
+
             expect(
                 isPostReq({ method: '' })
             ).to.not.be.ok();
-    
+
             expect(
                 isPostReq({
                     method: 'get'
-            })
+                })
             ).to.not.be.ok();
-      });
-      });
+        });
+    });
 
-      describe('isPutReq()', () => {
+    describe('isPutReq()', () => {
         it('should return true', () => {
             expect(
                 isPutReq({
                     method: 'put'
-            })
+                })
             ).to.be.ok();
 
             expect(
                 isPutReq({
                     method: 'Put'
-            })
+                })
             ).to.be.ok();
 
             expect(
                 isPutReq({
                     method: 'PUt'
-            })
+                })
             ).to.be.ok();
 
             expect(
                 isPutReq({
                     method: 'PUT'
-            })
+                })
             ).to.be.ok();
         });
 
@@ -263,51 +264,51 @@ describe('is-json-request util', () => {
             expect(
                 isPutReq()
             ).to.not.be.ok();
-    
+
             expect(
                 isPutReq({})
             ).to.not.be.ok();
-    
+
             expect(
                 isPutReq({ method: null })
             ).to.not.be.ok();
-    
+
             expect(
                 isPutReq({ method: '' })
             ).to.not.be.ok();
-    
+
             expect(
                 isPutReq({
                     method: 'get'
-            })
+                })
             ).to.not.be.ok();
-      });
-      });
+        });
+    });
 
-      describe('isDeleteReq()', () => {
+    describe('isDeleteReq()', () => {
         it('should return true', () => {
             expect(
                 isDeleteReq({
                     method: 'delete'
-            })
+                })
             ).to.be.ok();
 
             expect(
                 isDeleteReq({
                     method: 'Delete'
-            })
+                })
             ).to.be.ok();
 
             expect(
                 isDeleteReq({
                     method: 'DElete'
-            })
+                })
             ).to.be.ok();
 
             expect(
                 isDeleteReq({
                     method: 'DELETE'
-            })
+                })
             ).to.be.ok();
         });
 
@@ -315,24 +316,24 @@ describe('is-json-request util', () => {
             expect(
                 isDeleteReq()
             ).to.not.be.ok();
-    
+
             expect(
                 isDeleteReq({})
             ).to.not.be.ok();
-    
+
             expect(
                 isDeleteReq({ method: null })
             ).to.not.be.ok();
-    
+
             expect(
                 isDeleteReq({ method: '' })
             ).to.not.be.ok();
-    
+
             expect(
                 isDeleteReq({
                     method: 'get'
-            })
+                })
             ).to.not.be.ok();
-      });
-      });
-  });
+        });
+    });
+});
